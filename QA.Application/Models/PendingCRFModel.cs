7433fd5207dfc2878412fb5e71511876a888dc74
RAJENDRAN N <nrajendran497@gmail.com>
2026-01-17 16:18:17 +0530

    New Testcases page

---
+++ QA.Application/Models/PendingCRFModel.cs
@@ -0,0 +1,14 @@
+﻿namespace QA.Application.Models
+{
+    public class PendingCRFModel
+    {
+        public string CRFID { get; set; } = string.Empty;
+        public string REQUESTID { get; set; } = string.Empty;
+        public string Objective { get; set; } = string.Empty;
+        public DateTime? Testing_start_date { get; set; }
+        public DateTime? Testing_end_date { get; set; }
+        public string Developer { get; set; } = string.Empty;
+        public string Techlead { get; set; } = string.Empty;
+        public string STATUS { get; set; } = string.Empty;
+    }
+}
